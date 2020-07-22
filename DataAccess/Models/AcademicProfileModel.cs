e782fd1893f07741a9e42e5c1d56de462572d618
Imtiaj Ahammad <imtiajahammad.official@gmail.com>
2020-07-22 12:30:29 +0600

    AcademicProfileModel

---
+++ DataAccess/Models/AcademicProfileModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAccess.Models
+{
+    public class AcademicProfileModel
+    {
+        public int id { get; set; }
+        public SingleSmallTextModel institutionName { get; set; }
+        public SingleSmallTextModel location { get; set; }
+        public SingleSmallTextModel courseName { get; set; }
+        public SingleSmallTextModel cgpa { get; set; }
+        public SingleDateModel start { get; set; }
+        public SingleDateModel end { get; set; }
+        public LargeTextListDataModel briefPoint { get; set; }
+    }
+}
