fcb0a9f1772b920a1c6d02dc6a86911825b7527b
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2020-07-25 11:07:08 +0600

    UserGateway added

---
+++ DataAccess/DbGateways/UserGateway.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DataAccess.DbGateways
+{
+    public class UserGateway
+    {
+        
+        public int getCareerAdjectiveId_ByUserId (int userid)
+        {
+        int returnId=0;
+            using (SqlConnection aSqlConnection
+                = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "getCareerAdjectiveId_ByUserId";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("userId", userid);
+                    cmd.Connection = aSqlConnection;
+                    aSqlConnection.Open();
+                    SqlDataReader aSqlDataReader = cmd.ExecuteReader();
+                    while (aSqlDataReader.Read())
+                    {
+                        returnId = Convert.ToInt32(aSqlDataReader["id"].ToString());
+                    }                    
+                }
+            }
+            return returnId;
+        }
+        public int getProjectId_ByUserId (int userid)
+        {
+        int returnId=0;
+            using (SqlConnection aSqlConnection
+                = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "getProjectId_ByUserId";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("userId", userid);
+                    cmd.Connection = aSqlConnection;
+                    aSqlConnection.Open();
+                    SqlDataReader aSqlDataReader = cmd.ExecuteReader();
+                    while (aSqlDataReader.Read())
+                    {
+                        returnId = Convert.ToInt32(aSqlDataReader["id"].ToString());
+                    }                    
+                }
+            }
+            return returnId;
+        }
+        public int getContactId_ByUserId (int userid)
+        {
+        int returnId=0;
+            using (SqlConnection aSqlConnection
+                = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "getContactId_ByUserId";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("userId", userid);
+                    cmd.Connection = aSqlConnection;
+                    aSqlConnection.Open();
+                    SqlDataReader aSqlDataReader = cmd.ExecuteReader();
+                    while (aSqlDataReader.Read())
+                    {
+                        returnId = Convert.ToInt32(aSqlDataReader["id"].ToString());
+                    }                    
+                }
+            }
+            return returnId;
+        }
+        public int getAboutId_ByUserId (int userid)
+        {
+        int returnId=0;
+            using (SqlConnection aSqlConnection
+                = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "getAboutId_ByUserId";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("userId", userid);
+                    cmd.Connection = aSqlConnection;
+                    aSqlConnection.Open();
+                    SqlDataReader aSqlDataReader = cmd.ExecuteReader();
+                    while (aSqlDataReader.Read())
+                    {
+                        returnId = Convert.ToInt32(aSqlDataReader["id"].ToString());
+                    }                    
+                }
+            }
+            return returnId;
+        }
+        public int getSelfImageId_ByUserId (int userid)
+        {
+        int returnId=0;
+            using (SqlConnection aSqlConnection
+                = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "getSelfImageId_ByUserId";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("userId", userid);
+                    cmd.Connection = aSqlConnection;
+                    aSqlConnection.Open();
+                    SqlDataReader aSqlDataReader = cmd.ExecuteReader();
+                    while (aSqlDataReader.Read())
+                    {
+                        returnId = Convert.ToInt32(aSqlDataReader["id"].ToString());
+                    }                    
+                }
+            }
+            return returnId;
+        }
+        public int getExperienceId_ByUserId (int userid)
+        {
+        int returnId=0;
+            using (SqlConnection aSqlConnection
+                = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "getExperienceId_ByUserId";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("userId", userid);
+                    cmd.Connection = aSqlConnection;
+                    aSqlConnection.Open();
+                    SqlDataReader aSqlDataReader = cmd.ExecuteReader();
+                    while (aSqlDataReader.Read())
+                    {
+                        returnId = Convert.ToInt32(aSqlDataReader["id"].ToString());
+                    }                    
+                }
+            }
+            return returnId;
+        }
+        public int getSkillId_ByUserId(int userid)
+        {
+        int returnId=0;
+            using (SqlConnection aSqlConnection
+                = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "getSkillId_ByUserId";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("userId", userid);
+                    cmd.Connection = aSqlConnection;
+                    aSqlConnection.Open();
+                    SqlDataReader aSqlDataReader = cmd.ExecuteReader();
+                    while (aSqlDataReader.Read())
+                    {
+                        returnId = Convert.ToInt32(aSqlDataReader["id"].ToString());
+                    }                    
+                }
+            }
+            return returnId;
+        }
+        public int getAcademicId_ByUserId(int userid)
+        {
+        int returnId=0;
+            using (SqlConnection aSqlConnection
+                = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "getAcademicId_ByUserId";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("userId", userid);
+                    cmd.Connection = aSqlConnection;
+                    aSqlConnection.Open();
+                    SqlDataReader aSqlDataReader = cmd.ExecuteReader();
+                    while (aSqlDataReader.Read())
+                    {
+                        returnId = Convert.ToInt32(aSqlDataReader["id"].ToString());
+                    }                    
+                }
+            }
+            return returnId;
+        }
+        
+        public int GetCareerAdjectiveId_ByUserId(int userid)
+        {
+            int returnId=0;
+            using (SqlConnection aSqlConnection
+                = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "GetCareerAdjectiveId_ByUserId";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("userId", userid);
+                    cmd.Connection = aSqlConnection;
+                    aSqlConnection.Open();
+                    SqlDataReader aSqlDataReader = cmd.ExecuteReader();
+                    while (aSqlDataReader.Read())
+                    {
+                        returnId = Convert.ToInt32(aSqlDataReader["id"].ToString());
+                    }                    
+                }
+            }
+            return returnId;
+        }
+
+        
+    }
+}
