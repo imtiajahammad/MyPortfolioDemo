25db7d9193a5a4b8513f7a980842307e779a2e0b
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2020-07-26 07:35:52 +0600

    UserManager added

--- DataAccess/Managers/UserManager.cs
+++ DataAccess/Managers/UserManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess.DbGateways;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,46 @@ namespace DataAccess.Managers
 {
     public class UserManager
     {
-
+        UserGateway userGateway;
+        public UserManager()
+        {
+            this.userGateway = new UserGateway();
+        }
+        public int getCareerAdjectiveId_ByUserId(int userid)
+        {
+            return userGateway.getCareerAdjectiveId_ByUserId(userid);
+        }
+        public int getProjectId_ByUserId(int userid)
+        { 
+            return userGateway.getProjectId_ByUserId(userid);
+        }
+        public int getContactId_ByUserId(int userid)
+        { 
+            return userGateway.getContactId_ByUserId(userid);
+        }
+        public int getAboutId_ByUserId(int userid)
+        { 
+            return userGateway.getAboutId_ByUserId(userid);
+        }
+        public int getSelfImageId_ByUserId(int userid)
+        { 
+            return userGateway.getSelfImageId_ByUserId(userid);
+        }
+        public int getExperienceId_ByUserId(int userid)
+        { 
+            return userGateway.getExperienceId_ByUserId(userid);
+        }
+        public int getSkillId_ByUserId(int userid)
+        { 
+            return userGateway.getSkillId_ByUserId(userid);
+        }
+        public int getAcademicId_ByUserId(int userid)
+        { 
+            return userGateway.getAcademicId_ByUserId(userid);
+        }
+        public int GetCareerAdjectiveId_ByUserId(int userid)
+        { 
+            return userGateway.GetCareerAdjectiveId_ByUserId(userid);
+        }
     }
 }
