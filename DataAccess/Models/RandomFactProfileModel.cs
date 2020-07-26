1698232238a63ef78a16d625977708344f7c92b4
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2020-07-26 08:09:11 +0600

    Model and profileModel added for accomplishment,Blog,RandomFact,ToolsIUse.

---
+++ DataAccess/Models/RandomFactProfileModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAccess.Models
+{
+    public class RandomFactProfileModel
+    {
+        public int id { get; set; }
+    }
+}
---
+++ DataAccess/Models/ToolsIUseProfileModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAccess.Models
+{
+    public class ToolsIUseProfileModel
+    {
+        public int id { get; set; }
+    }
+}
